19a0e481154f7c6e64ad7bd904abe516df257c0f
fortijames <forti.james@gmail.com>
2022-02-24 08:34:27 -0500

    updated author name

--- scripts/create-system-aliases-for-tunnels.tcl
+++ scripts/create-system-aliases-for-tunnels.tcl
@@ -4,7 +4,7 @@
 ###Find VPNs and create an Alias Command for Each Tunnel
 ##
 ##History:
-#     20210218 - Initially created so I can tab-complete my common VPN commands. - jhilving@fortinet.com
+#     20210218 - Initially created so I can tab-complete my common VPN commands. - fortijames
 #
 #-------------------------------------------------------------------
 
